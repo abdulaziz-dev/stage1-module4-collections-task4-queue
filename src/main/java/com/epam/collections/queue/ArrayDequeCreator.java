ce35f2e464f895373d8d3e20bcb1199ee68a1c72
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2023-03-28 13:17:14 +0500

    Queues

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,24 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(firstQueue.remove());
+        deque.add(firstQueue.remove());
+        deque.add(secondQueue.remove());
+        deque.add(secondQueue.remove());
+        boolean turn = true;
+        while (!(firstQueue.isEmpty() && secondQueue.isEmpty())){
+            if(turn){
+                firstQueue.add(deque.pollLast());
+                deque.add(firstQueue.remove());
+                deque.add(firstQueue.remove());
+            } else {
+                secondQueue.add(deque.pollLast());
+                deque.add(secondQueue.remove());
+                deque.add(secondQueue.remove());
+            }
+            turn = !turn;
+        }
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,34 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> ints = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            ints.add(i);
+        }
+        int step = 1, i = 0;
+
+        while (ints.size() != 0) {
+            if (i < ints.size()) {
+                if (step == everyDishNumberToEat) {
+                    list.add(ints.get(i));
+                    ints.remove(ints.get(i));
+                    step = 1;
+                } else {
+                    step++;
+                    i++;
+                }
+            } else i = 0;
+
+            if (ints.size() == 1) {
+                list.add(ints.get(0));
+                ints.remove(ints.get(0));
+            }
+        }
+        return list;
     }
 }
